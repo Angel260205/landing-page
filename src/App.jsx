66d01d6e76c8d09f94e7d2c1cc50a7ac4b87533f
Angel Pérez Poot <perezpootangelgabriel@gmail.com>
2024-09-17 22:02:51 -0600

    orden de Archivos

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import Header from './components/Header';
+import Contenido from './components/Contenido';
+import ImageGallery from './components/ImageGallery';
+import Footer from './components/Footer';
+import './estilo.css'; 
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
- <div>
-  hola como estas el dia de hoy 
- </div>
+    <div>
+    <Header />
+    <Contenido />
+    <ImageGallery />
+    <Footer />
+  </div>
   )
 }
 
