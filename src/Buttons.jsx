15d03767636469f90166b65069f0b48e092b3492
Angel Pérez Poot <perezpootangelgabriel@gmail.com>
2024-09-17 21:08:10 -0600

    Implementacion Botones

---
+++ src/Buttons.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Buttons() {
+  return (
+    <div className="buttons">
+      <button>Contenido</button>
+      <button>Comprar</button>
+      <button>Mas Historia</button>
+    </div>
+  );
+}
+
+export default Buttons;
