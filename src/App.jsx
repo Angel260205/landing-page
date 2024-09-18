e1afe41675b6a6fdf701538090ab1bdf75cda4b7
Angel Pérez Poot <perezpootangelgabriel@gmail.com>
2024-09-17 20:45:22 -0600

    Eliminando submódulo landing-page

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+ <div>
+  hola como estas el dia de hoy 
+ </div>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Contáctanos</a></li>
+          <li><a href="#">Personajes</a></li>
+          <li><a href="#">Regístrate</a></li>
+          <li><a href="#">Cuenta</a></li>
+        </ul>
+        <div className="theme-toggle">
+          <input type="checkbox" id="dark-mode-toggle" />
+          <label htmlFor="dark-mode-toggle"></label>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
